3e88b694e520beb7fd85cd08ef63c639c3b31e02
callingsandesh <sandeshtimalsina@gmail.com>
2020-07-05 07:26:16 +0545

    2:02:37

--- tweetme2/tweets/views.py
+++ tweetme2/tweets/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 import random
 
 from django.http import  HttpResponse,Http404 , JsonResponse
@@ -17,7 +17,22 @@ def home_view(request , *args , **kwargs):
     return render(request, "pages/home.html" ,context={},status=200)
 
 def tweet_create_view(request,*args,**kwargs):
-    
+    form=TweetForm(request.POST or None)
+    #print('post data is :',request.POST)
+
+    next_url=request.POST.get("next") or None
+    print("next_url",next_url)
+    if form.is_valid():
+        obj=form.save(commit=False)
+        #do other form related logic
+        obj.save()
+        if next_url != None:
+            return redirect(next_url)
+
+
+        form=TweetForm()
+        
+    return render(request,'components/form.html',context={"form":form})
 
 def tweet_list_view(request,*args,**kwargs):
     qs=Tweet.objects.all()
