39e8a820227bd5d5b21025400a50c72d7a043bee
callingsandesh <sandeshtimalsina@gmail.com>
2020-07-05 21:51:16 +0545

    added in INSTALLED_APPS

--- tweetme2/tweetme2/settings.py
+++ tweetme2/tweetme2/settings.py
@@ -43,7 +43,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tweets'
+    #Third-party
+    'rest_framework',
+    #internal
+    'tweets',
 ]
 
 MIDDLEWARE = [
