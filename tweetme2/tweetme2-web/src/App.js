f06f1ce1d5293ac7ec64daa053836c15b5eecdd9
callingsandesh <sandeshtimalsina@gmail.com>
2020-07-08 08:28:24 +0545

    hello world react

--- tweetme2/tweetme2-web/src/App.js
+++ tweetme2/tweetme2-web/src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+  const [tweets , setTweets] = useState([{content: 123}])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          {tweets.map((tweet,index)=>{
+            return <li>{tweet.content}</li>
+          })}
         </p>
         <a
           className="App-link"
