f0673228db535d707c349b77f1c9f3b875690ee9
callingsandesh <sandeshtimalsina@gmail.com>
2020-07-11 12:12:22 +0545

    5:42:52

--- tweetme2/tweetme2/urls.py
+++ tweetme2/tweetme2/urls.py
@@ -17,13 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 from tweets.views import  (
-
     home_view,
     tweet_detail_view,
     tweet_list_view,
     tweet_create_view,
-    tweet_delete_view,
-    tweet_action_view
+    #tweet_delete_view,
+    #tweet_action_view
 
     )
 
