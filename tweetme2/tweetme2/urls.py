b85db3d1fb517c1041fbd7710b070ffcb5ec7313
callingsandesh <sandeshtimalsina@gmail.com>
2020-07-11 08:54:36 +0545

    urls

--- tweetme2/tweetme2/urls.py
+++ tweetme2/tweetme2/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from tweets.views import  (
 
@@ -32,9 +32,11 @@ urlpatterns = [
     path('', home_view),
     path('tweets/', tweet_list_view),
     path('create-tweet/', tweet_create_view),
-    path('tweets/<int:tweet_id>', tweet_detail_view),
-    path('api/tweets/<int:tweet_id>/delete', tweet_delete_view),
-    path('api/tweets/action', tweet_action_view),
+    path('tweets/<int:tweet_id>/', tweet_detail_view),
+    #path('api/tweets/<int:tweet_id>/delete', tweet_delete_view),
+    #path('api/tweets/action', tweet_action_view),
+
+    path('api/tweets/',include('tweets.urls'))
     
 
 
