da90efd2db927a3238ea949527ef775b70e9e872
callingsandesh <sandeshtimalsina@gmail.com>
2020-07-11 10:07:53 +0545

    tweets render sucessfully by react

--- tweetme2/tweetme2-web/src/App.js
+++ tweetme2/tweetme2-web/src/App.js
@@ -7,7 +7,7 @@ function loadTweets(callback){
         
   const xhr=new XMLHttpRequest()
   const method ='GET'  //"POST"
-  const url="http://127.0.0.1:8000/api/tweets/"
+  const url="http://localhost:8000/api/tweets/"
   const responseType="json"
   xhr.responseType = responseType
   xhr.open(method,url)
@@ -19,7 +19,10 @@ function loadTweets(callback){
 
   //console.log(listedItems)
 }
-
+xhr.onerror = function(e){
+  console.log(e)
+  callback({"message":"The request was an error"},400)
+}
 xhr.send()
 
 }
@@ -34,6 +37,8 @@ function App() {
     const myCallback = (response,status)=>{
       if(status === 200){
         setTweets(response)
+      }else{
+        alert("Their was an error")
       }
     
     
