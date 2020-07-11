19db8448876dd7c0d95b8eaa74050c94e964467f
callingsandesh <sandeshtimalsina@gmail.com>
2020-07-11 08:40:32 +0545

    5:35

--- tweetme2/tweetme2-web/src/App.js
+++ tweetme2/tweetme2-web/src/App.js
@@ -3,9 +3,46 @@ import logo from './logo.svg';
 import './App.css';
 
 
+function loadTweets(callback){
+        
+  const xhr=new XMLHttpRequest()
+  const method ='GET'  //"POST"
+  const url="http://localhost:3000/tweets/"
+  const responseType="json"
+  xhr.responseType = responseType
+  xhr.open(method,url)
+ 
+
+  xhr.onload =function(){
+  callback(xhr.response, xhr.status)
+}
+xhr.onerror = function(e){
+  console.log(e)
+  callback({"message":"The request was an error"},400)
+}
+xhr.send()
+
+}
+
 
 function App() {
-  const [tweets , setTweets] = useState([{content: 123}])
+  const [tweets , setTweets] = useState([])
+
+
+  
+  useEffect(()=>{
+    //do my lookup
+    const myCallback = (response , status)=>{
+      console.log(response,status)
+      if (status === 200){
+        setTweets(response)
+      }else{
+        alert("Their was an error")
+      }
+    }
+    loadTweets(myCallback)
+    
+  },[])
   return (
     <div className="App">
       <header className="App-header">
--- tweetme2/tweetme2/settings.py
+++ tweetme2/tweetme2/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 
 
 #implicitly added
-ALLOWED_HOSTS = ['127.0.0.1','.cfe.sh']
+ALLOWED_HOSTS = ['127.0.0.1','.cfe.sh','localhost']
 LOGIN_URL='/login'
 MAX_TWEET_LENGTH = 250
 TWEET_ACTION_OPTIONS =["like","unlike","retweet"]
@@ -47,14 +47,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #Third-party
+    'corsheaders',
     'rest_framework',
     #internal
     'tweets',
+
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -131,6 +134,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+CORS_ORIGIN_ALLOW_ALL = True   #any website has access to my api
+CORS_URLS_REGEX = r'^/api/.*$'
 
 DEFAULT_RENDERER_CLASSES=[
         'rest_framework.renderers.JSONRenderer',
